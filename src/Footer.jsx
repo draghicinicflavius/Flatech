583c4794c825302174629e590f58eb5a2138f1da
Flavius Draghici <draghici.nic.flavius@gmail.com>
2026-03-03 20:26:11 +0200

    3

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', textAlign: 'center', borderTop: '1px solid #eee', marginTop: '50px' }}>
+      <p>&copy; 2026 Numele Firmei Tale. Toate drepturile rezervate.</p>
+      <small style={{ color: '#888' }}>Application by Flavius Draghici</small>
+    </footer>
+  );
+}
+
+export default Footer;
